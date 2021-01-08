fa4dec3b775d0b343395597e1883f923af52c2f8
anhtudo97 <anhtudo97@gmail.com>
2021-01-08 23:03:04 +0700

    feat: demo model interface

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
+import { Task } from './task.model';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TasksService {
-    private tasks = []
+  private tasks: Task[] = [];
 
-    getAllTasks(){
-        return this.tasks
-    }
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 }
