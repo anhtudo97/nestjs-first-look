f43881fc093b02e210af6c4f074c480bd59d8ef9
Do Anh Tu <anhtudo97@gmail.com>
2021-01-11 14:42:58 +0700

    feat: auth sign up

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,16 @@
+import { AuthCredentialDto } from './dto/auth-credentials.dto';
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+
+    await user.save();
+  }
+}
